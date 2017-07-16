ff96d57c813407a17f20709a971e282f2e071252
Cameron Irmas <cjirmas@gmail.com>
2017-07-16 12:02:16 -0700

    Initial commit

---
+++ lib/distributed_messages.ex
@@ -0,0 +1,18 @@
+defmodule DistributedMessages do
+  @moduledoc """
+  Documentation for DistributedMessages.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DistributedMessages.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/distributed_messages_test.exs
@@ -0,0 +1,8 @@
+defmodule DistributedMessagesTest do
+  use ExUnit.Case
+  doctest DistributedMessages
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
